2a11ef9487ba56b1abbbe8a96c75fcd3465be420
Dennis Nguyen <dennisdnguyen19@gmail.com>
2020-02-25 08:47:08 -0500

    comments

--- deploy.sh
+++ deploy.sh
@@ -9,19 +9,25 @@ $SUDO apt update
 $SUDO apt install default-jre -y
 
 cd ..
+
+#Build Spigot
 wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 java -jar BuildTools.jar
+
+#Create folder containing world and copy necessary files
+#World will be named after currently logged in user
 worldName=$USER
 mkdir $worldName
 rsync -a MinecraftUtils/start.sh $worldName
 
-for file in /*
+for file in ./*
 do
 if grep -q spigot "$file"; then
 rsync -a $file $worldName/spigot.jar
 fi
 done
 
+#First run of Spigot and accept EULA
 cd $worldName
 java -jar -Xms1G -Xmx1G -jar spigot.jar
 sed 's/false/true/' eula.txt
